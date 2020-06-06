92bb7275986906dc4fec283ba394d1e6203ddccc
asultan00 <asultan00@stuy.edu>
2020-06-06 12:36:29 -0400

    flask routes

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,46 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 
 app = Flask(__name__)
 
+# LANDING PAGE
 @app.route("/")
 def root():
      return render_template("landing.html")
+     # eventually, will go to some kind of homepage or redirect to opportunity bulletin if logged in already
 
-@app.route("/homepage")
-def homepage():
+# OPPORTUNITY BULLETIN
+@app.route("/opportunities")
+def opportunities():
      return render_template("index.html")
+
+# INDIVIDUAL OPPORTUNITY
+@app.route("/opportunities/<opportunityID>")
+def opportunity():
+    return 'placeholder'
+
+# SCHOLARSHIP FINDER
+@app.route("/scholarships")
+def scholarships():
+    return 'placeholder'
+
+# INDIVIDUAL SCHOLARSHIP
+@app.route("/scholarships/<scholarshipID>")
+def scholarship():
+    return 'placeholder'
+
+# FAVORITES/SAVED
+@app.route("/favorites")
+def favorites():
+    return 'placeholder'
+
+# RESOURCES
+@app.route("/resources")
+def resources():
+    return 'placeholder'
+
+# PREFERENCES
+@app.route("/preferences")
+def preferences():
+    return 'placeholder'
 
 if __name__ == "__main__":
     app.debug = True
