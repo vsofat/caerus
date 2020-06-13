14421912b06dc13853eb4fb54aca599bcb89f617
ray. lee <rlee00@stuy.edu>
2020-06-13 01:15:35 -0400

    Separate app creation into create_app fxn

--- app/test/database/functions/search/searchResources_test.py
+++ app/test/database/functions/search/searchResources_test.py
@@ -1,5 +1,23 @@
-def test_always_passes():
-    assert True
+import pytest
 
-def test_always_fails():
-    assert False
+from app.__init__ import app, db
+from app.utl.database.models.models import *
+
+
+@pytest.fixture
+def setUp():
+    db_uri = "sqlite:///:memory:"
+    app.config["TESTING"] = True
+    app.config["WTF_CSRF_ENABLED"] = False
+    app.config["SQL_ALCHEMY_DATABASE_URI"] = db_uri
+    appTestClient = app.test_client()
+    db.create_all(appTestClient)
+
+@pytest.fixture
+def tearDown():
+    db.session.remove()
+    db.drop_all()
+
+def test_db_connection():
+    setUp
+    db.session.commit()
