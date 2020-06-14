7e36117f55babf24956d08871dee1eb41aaf4c5d
ray. lee <rlee00@stuy.edu>
2020-06-13 23:36:15 -0400

    Test search resources

--- app/test/database/functions/find/findResources_test.py
+++ app/test/database/functions/find/findResources_test.py
@@ -62,6 +62,16 @@ def test_sortResources(session):
         baseQuery, datePostedDescSortString
     )
     sortedResourcesListByDatePostedDesc = sortedResourcesQueryByDatePostedDesc.all()
+    # sort was not provided or provided as None
+    sortedResourcesQueryByDatePostedNone = sortResources(
+        baseQuery, None
+    )
+    sortedResourcesListByDatePostedNone = sortedResourcesQueryByDatePostedNone.all()
+    # sort was not provided or provided as ""
+    sortedResourcesQueryByDatePostedEmptyQuotes = sortResources(
+        baseQuery, ""
+    )
+    sortedResourcesListByDatePostedEmptyQuotes = sortedResourcesQueryByDatePostedNone.all()
 
     # assert
     for idx, resource in enumerate(sortedResourcesListByDatePostedAsc):
@@ -70,6 +80,61 @@ def test_sortResources(session):
     for idx, resource in enumerate(sortedResourcesListByDatePostedDesc):
         assert resource.datePosted == sortedTResourcesListByDatePostedDesc[idx].datePosted
 
+    for idx, resource in enumerate(sortedResourcesListByDatePostedNone):
+        assert resource.datePosted == sortedTResourcesListByDatePostedAsc[idx].datePosted
+
+    for idx, resource in enumerate(sortedResourcesListByDatePostedEmptyQuotes):
+        assert resource.datePosted == sortedTResourcesListByDatePostedAsc[idx].datePosted
+
 
 def test_searchResources(session):
-    assert True
+    baseQuery = Resource.query
+    search = "ff"
+    datePostedAscSortString = "dateposted-asc"
+    datePostedDescSortString = "dateposted-desc"
+
+    # arrange
+    tResource1 = Resource(title="ff", description="faf", link="fif")
+    tResource2 = Resource(title="ff", description="gag", link="gig")
+    tResource3 = Resource(title="hh", description="hah", link="hih")
+    tResourcesList = [tResource1, tResource2, tResource3]
+    session.add(tResource1)
+    session.add(tResource2)
+    session.add(tResource3)
+
+    # act
+    # Non-empty search query
+    searchResourcesQueryNonEmpty = searchResources(baseQuery, search)
+    searchResourcesResultsNonEmpty = searchResourcesQueryNonEmpty.all()
+    # None as search query
+    searchResourcesQueryNone = searchResources(baseQuery, None)
+    searchResourcesResultsNone = searchResourcesQueryNone.all()
+    # Empty search query
+    searchResourcesQueryEmpty = searchResources(baseQuery, "")
+    searchResourcesResultsEmpty = searchResourcesQueryEmpty.all()
+
+    # assert
+    # Assume that searchResourcesResultsNonEmpty is in no particular order because order is outside of this fxn's concern.
+    # Non-empty search query
+    assert isinstance(searchResourcesResultsNonEmpty, list)
+    assert len(searchResourcesResultsNonEmpty) == 2
+    assert tResource1 in searchResourcesResultsNonEmpty
+    assert tResource2 in searchResourcesResultsNonEmpty
+    # TODO: Sometimes, if the datePosted value is identical, the order of resource retrieval could be [<Resource 1>, <Resource 2>] or [<Resource 2>, <Resource 1>], so the check below may be faulty.
+    assert searchResourcesResultsNonEmpty == [tResource1, tResource2]
+
+    # None as search query
+    assert isinstance(searchResourcesResultsNone, list)
+    assert len(searchResourcesResultsNone) == 3
+    assert tResource1 in searchResourcesResultsNone
+    assert tResource2 in searchResourcesResultsNone
+    # TODO: Sometimes, if the datePosted value is identical, the order of resource retrieval could be [<Resource 1>, <Resource 2>] or [<Resource 2>, <Resource 1>], so the check below may be faulty.
+    assert searchResourcesResultsNone == [tResource1, tResource2, tResource3]
+
+    # Empty search query
+    assert isinstance(searchResourcesResultsEmpty, list)
+    assert len(searchResourcesResultsEmpty) == 3
+    assert tResource1 in searchResourcesResultsEmpty
+    assert tResource2 in searchResourcesResultsEmpty
+    # TODO: Sometimes, if the datePosted value is identical, the order of resource retrieval could be [<Resource 1>, <Resource 2>] or [<Resource 2>, <Resource 1>], so the check below may be faulty.
+    assert searchResourcesResultsEmpty == [tResource1, tResource2,tResource3]
--- app/utl/database/functions/find/findResources.py
+++ app/utl/database/functions/find/findResources.py
@@ -2,6 +2,39 @@ from utl.database.models.models import db, Resource
 from sqlalchemy import or_
 
 
+def sortResources(baseQuery, sort):
+    sortOptionQueries = {
+        "dateposted-asc": Resource.datePosted.asc(),
+        "dateposted-desc": Resource.datePosted.desc(),
+    }
+
+    # default sort option
+    sortOptionQuery = "dateposted-asc"
+
+    # Check if sort is a truey value (i.e. not None or "") and is a key in sortOptionQueries
+    if sort and sort in sortOptionQueries.keys():
+        sortOptionQuery = sort
+
+    sortedResources = baseQuery.order_by(sortOptionQueries[sortOptionQuery])
+
+    return sortedResources
+
+
+def searchResources(baseQuery, search):
+    # If search is None or ""
+    if not search:
+        return Resource.query
+
+    search = search.strip()
+    searchQueryString = "%" + search + "%"
+    
+    searchQuery = Resource.query.filter(
+        or_(Resource.title.ilike(searchQueryString), Resource.description.ilike(searchQueryString))
+    )
+
+    return searchQuery
+
+
 def findResources(body):
     """
     input:
@@ -14,27 +47,12 @@ def findResources(body):
     """
     search = body["search"]
     sort = body["sort"]
+    resources = None
 
     if search == "":
         baseQuery = Resource.query
-        return body, sortResources(baseQuery, sort)
+        resources = sortResources(baseQuery, sort).all()
     else:
-        return body, searchResources(sortResources(baseQuery, sort), search)
+        resources = searchResources(sortResources(baseQuery, sort), search).all()
 
-
-def sortResources(baseQuery, sort):
-    sortOptionQueries = {
-        "dateposted-asc": Resource.datePosted.asc(),
-        "dateposted-desc": Resource.datePosted.desc(),
-    }
-    sortedResources = baseQuery.order_by(sortOptionQueries[sort])
-
-    return sortedResources
-
-
-def searchResources(baseQuery, search):
-    like = "%" + search + "%"
-    searchQuery = Resource.query.filter(
-        or_(Resource.title.like(like), Resource.description.like(like))
-    )
-    return searchQuery
+    return body, resources
