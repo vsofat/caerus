17be2f934ff72c6b751152acc52b165192f5c5c0
ericlau00 <ericlau41802@gmail.com>
2020-06-15 01:25:49 -0400

    Parameterize AJAX request for favorites

--- app/static/js/favorite.js
+++ app/static/js/favorite.js
@@ -1,11 +1,17 @@
-let favorite = (id) => {
-    console.log(id);
+let favorite = (type, id) => {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
+            // let flash = `
+            // <div class="sidenav alert alert-success text-align-center alert-dismissible fade show" role="alert">
+            //     ${this.responseText}
+            //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            //         <span aria-hidden="true">&times;</span>
+            //    </button>
+            // </div>`;
+            // document.body.innerHTML += flash;
         }
     };
-    xhttp.open('GET', `/favorite/opportunity/${id}`, true);
+    xhttp.open('GET', `/favorite/${type}/${id}`, true);
     xhttp.send();
 };
