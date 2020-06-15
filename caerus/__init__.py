f8f92a3224a9143c6ba72f460d8567fee279a25c
Kazi Jamal <jamalkazi02@gmail.com>
2020-06-15 11:22:42 -0400

    Update __init__.py

--- caerus/__init__.py
+++ caerus/__init__.py
@@ -501,5 +501,5 @@ if __name__ == "__main__":
     db.init_app(app)
     with app.app_context():
         db.create_all()
-    app.debug = True
-    app.run()
+    app.debug = False
+    app.run(host='0.0.0.0')
