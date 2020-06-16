cd5d854b444998d16699043416418d7141620cd4
asultan00 <asultan00@stuy.edu>
2020-06-15 23:56:40 -0400

    added delete, edit buttons, fixed functions

--- app/static/js/delete.js
+++ app/static/js/delete.js
@@ -1,14 +1,16 @@
-let deleteObject = (type, id) => {
-    let confirmation = confirm(`Are you sure you want to delete this ${type}?`);
+let deleteObject = (t, id) => {
+    let confirmation = confirm(`Are you sure you want to delete this ${t}?`);
+    console.log(confirmation);
     if (confirmation) {
         let xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = async function() {
+        xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
-                let obj = document.getElementById(`${type}-${id}`);
+                let obj = document.getElementById(`${id}`);
                 obj.parentNode.removeChild(obj);
+                console.log("aiahrtqjir3j");
             }
         };
-        xhttp.open('GET', `/delete/${type}/${id}`, true);
+        xhttp.open('GET', `/delete/${t}/${id}`, true);
         xhttp.send();
     }
 };
